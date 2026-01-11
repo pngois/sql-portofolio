f792acee1ea18c88f9c8945c82ac229708a48dac
Pedro Santos <135766062+pngois@users.noreply.github.com>
2026-01-11 15:32:35 +0000

    Update inserts.sql

--- online-store-project/inserts.sql
+++ online-store-project/inserts.sql
@@ -15,7 +15,11 @@ INSERT INTO orders VALUES
 (2),
 (1);
 
-INSERT INTO items_orders
+INSERT INTO items_orders (orders_id, products_id, quantity, price_unity) VALUES
+(1, 1, 2, 15.99),  -- 2 ratos
+(1, 2, 1, 29.99),  -- 1 teclado
+(2, 3, 1, 149.99), -- 1 monitor
+(3, 4, 2, 49.99);  -- 2 headphones
 
 
 
