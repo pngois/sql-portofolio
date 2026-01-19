0a75dbd510fa115003a0f71c7754ee6de90415a3
Pedro Santos <135766062+pngois@users.noreply.github.com>
2026-01-19 20:45:43 +0000

    Create data cleaning SQL script and analytics view
    Added SQL queries for data cleaning and analytics view.

---
+++ Netflix Users Analytic/SQL/data_cleaning.sql
@@ -0,0 +1,33 @@
+USE netflixuser;
+
+-- Test the table to check any Null value
+SELECT * FROM netflix_users 
+WHERE User_ID IS NULL 
+   OR Age IS NULL 
+   OR Country IS NULL 
+   OR Watch_Time_Hours IS NULL;
+   
+-- Test to check if any country is written in different ways
+SELECT Country, COUNT(*) FROM netflix_users GROUP BY Country;
+
+-- Test to check if any subscrition type is written in different ways
+SELECT Subscription_Type, COUNT(*) FROM netflix_users GROUP BY Subscription_Type;
+
+-- Create a view 
+CREATE OR REPLACE VIEW vw_netflix_analytics AS 
+SELECT User_ID,
+Age,
+CASE 
+        WHEN Age < 25 THEN '18-24 (Gen Z)'        
+        WHEN Age BETWEEN 25 AND 40 THEN '25-40 (Millennial)'
+        WHEN Age BETWEEN 41 AND 55 THEN '41-55 (Gen X)'
+        ELSE '55+ (Boomer)'
+    END AS Age_Group, -- Creating Age groups for analisys  
+ Last_Login,
+ Country, 
+ Watch_Time_Hours,
+ Subscription_Type
+ FROM netflix_users;
+ 
+ -- Check the view 
+  SELECT * FROM vw_netflix_analytics LIMIT 100;-- Test
